f6cfaf30836894933470a5cb73981fda4917a635
Farizki Kurniawan <fariski.mail@gmail.com>
2024-03-13 17:21:30 +0700

    feat: delete transaction

--- app/src/main/java/com/example/bondoman/ui/transaction/TransactionFragment.kt
+++ app/src/main/java/com/example/bondoman/ui/transaction/TransactionFragment.kt
@@ -1,15 +1,22 @@
 package com.example.bondoman.ui.transaction
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
@@ -28,6 +35,10 @@ class TransactionFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -41,6 +52,35 @@ class TransactionFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                // Add menu items here
+                menuInflater.inflate(R.menu.transaction_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                // Handle the menu selection
+                when (menuItem.itemId) {
+                    R.id.deleteTransaction -> {
+                        if(context != null && transactionId != 0L){
+                            AlertDialog.Builder(context!!)
+                                .setTitle("Delete Transaction")
+                                .setMessage("Are you sure you want to delete this transaction?")
+                                .setPositiveButton("Yes") {dialogInterface ,i ->
+                                    viewModel.deleteTransaction(currentTransaction)
+                                }
+                                .setNegativeButton("Cancel"){dialogInterface ,i ->}
+                                .create()
+                                .show()
+                        }
+
+                    }
+                }
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
 
 //        viewModel = ViewModelProvider(this)[TransactionViewModel::class.java]
         arguments?.let {
@@ -73,7 +113,7 @@ class TransactionFragment : Fragment() {
             Log.d("Observer", "Observer triggered with yippie")
             if(it) {
                 Toast.makeText(context, "Done!", Toast.LENGTH_SHORT).show()
-//                Navigation.findNavController(binding.titleView).popBackStack()
+                Navigation.findNavController(binding.titleView).popBackStack()
             } else{
                 Toast.makeText(context, "Something went wrong, please try again!", Toast.LENGTH_SHORT).show()
             }
