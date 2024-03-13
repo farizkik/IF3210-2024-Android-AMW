508f55cbe5facaaab23162015fbeaf364ad38979
Farizki Kurniawan <fariski.mail@gmail.com>
2024-03-13 12:06:47 +0700

    feat: transaction abstraction

--- app/src/main/java/com/example/bondoman/core/repository/TransactionDataSource.kt
+++ app/src/main/java/com/example/bondoman/core/repository/TransactionDataSource.kt
@@ -1,4 +1,14 @@
 package com.example.bondoman.core.repository
 
+import com.example.bondoman.core.data.Transaction
+
+
 interface TransactionDataSource {
+    suspend fun add(transaction: Transaction)
+
+    suspend fun get(id:Long): Transaction?
+
+    suspend fun getAll(): List<Transaction>
+
+    suspend fun remove(transaction: Transaction)
 }
--- app/src/main/java/com/example/bondoman/core/repository/TransactionRepository.kt
+++ app/src/main/java/com/example/bondoman/core/repository/TransactionRepository.kt
@@ -1,4 +1,13 @@
 package com.example.bondoman.core.repository
 
-class TransactionRepository {
+import com.example.bondoman.core.data.Transaction
+
+class TransactionRepository(private val dataSource: TransactionDataSource) {
+    suspend fun  addTransaction(transaction: Transaction) = dataSource.add(transaction)
+
+    suspend fun getTransaction(id: Long) = dataSource.get(id)
+
+    suspend fun getAllTransactions() = dataSource.getAll()
+
+    suspend fun removeTransaction(transaction: Transaction) = dataSource.remove(transaction)
 }
