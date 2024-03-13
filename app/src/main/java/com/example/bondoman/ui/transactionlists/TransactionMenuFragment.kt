4897ba8fc92d7fd328024929af55435fcc5690d2
Farizki Kurniawan <fariski.mail@gmail.com>
2024-03-13 12:03:52 +0700

    feat: transaction list design

--- app/src/main/java/com/example/bondoman/ui/transactionlists/TransactionMenuFragment.kt
+++ app/src/main/java/com/example/bondoman/ui/transactionlists/TransactionMenuFragment.kt
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.bondoman.databinding.FragmentDashboardBinding
 
-class DashboardFragment : Fragment() {
+class TransactionMenuFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
 
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val transactionMenuViewModel =
+            ViewModelProvider(this).get(TransactionMenuViewModel::class.java)
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        transactionMenuViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
