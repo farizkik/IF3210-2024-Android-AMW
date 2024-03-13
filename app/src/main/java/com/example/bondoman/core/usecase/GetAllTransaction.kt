06da33949b6065dde046b68ef21e8cc7c619cd2f
Farizki Kurniawan <fariski.mail@gmail.com>
2024-03-13 12:07:05 +0700

    feat: usecases

--- app/src/main/java/com/example/bondoman/core/usecase/GetAllTransaction.kt
+++ app/src/main/java/com/example/bondoman/core/usecase/GetAllTransaction.kt
@@ -2,6 +2,6 @@ package com.example.bondoman.core.usecase
 
 import com.example.bondoman.core.repository.TransactionRepository
 
-class GetTransaction (private val transactionRepository: TransactionRepository){
-    suspend operator fun invoke(id:Long) = transactionRepository.getTransaction(id)
+class GetAllTransaction (private val transactionRepository: TransactionRepository){
+    suspend operator fun invoke() = transactionRepository.getAllTransactions()
 }
