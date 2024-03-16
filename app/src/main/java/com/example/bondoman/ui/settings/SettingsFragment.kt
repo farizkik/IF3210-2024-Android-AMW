ee8d0f273884b92ab6b391c0b051b1e70d0611e6
Farizki Kurniawan <fariski.mail@gmail.com>
2024-03-16 12:32:54 +0700

    feat: settings fragment

---
+++ app/src/main/java/com/example/bondoman/ui/settings/SettingsFragment.kt
@@ -0,0 +1,44 @@
+package com.example.bondoman.ui.settings
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.example.bondoman.R
+import com.example.bondoman.databinding.FragmentNotificationsBinding
+import com.example.bondoman.databinding.FragmentSettingsBinding
+import com.example.bondoman.ui.notifications.NotificationsViewModel
+
+class SettingsFragment : Fragment() {
+
+    private var _binding: FragmentSettingsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    private lateinit var viewModel: SettingsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val viewModel =
+            ViewModelProvider(this).get(SettingsViewModel::class.java)
+
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
