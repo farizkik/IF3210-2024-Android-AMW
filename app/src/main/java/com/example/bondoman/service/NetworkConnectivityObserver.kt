6772f32952b70be89fb8c41cafe68b4354361175
Tansil011019 <13521054@std.stei.itb.ac.id>
2024-03-20 22:50:38 +0700

    fix: network sensing on intial running state

--- app/src/main/java/com/example/bondoman/service/NetworkConnectivityObserver.kt
+++ app/src/main/java/com/example/bondoman/service/NetworkConnectivityObserver.kt
@@ -3,6 +3,7 @@ package com.example.bondoman.service
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.Network
+import android.net.NetworkCapabilities
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
@@ -17,6 +18,17 @@ class NetworkConnectivityObserver (
 
     override fun observe(): Flow<ConnectivityObserver.Status> {
         return callbackFlow {
+
+            val network = connectivityManager.activeNetwork
+
+            val capabilities = connectivityManager.getNetworkCapabilities(network)
+
+            if (capabilities != null && capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)) {
+                launch { send(ConnectivityObserver.Status.Available) }
+            } else {
+                launch { send(ConnectivityObserver.Status.Unavailable) }
+            }
+
             val callback = object : ConnectivityManager.NetworkCallback() {
                 override fun onAvailable(network: Network) {
                     super.onAvailable(network)
