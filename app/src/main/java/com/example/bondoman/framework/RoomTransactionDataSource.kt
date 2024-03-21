827eb80fac7f6138fb294ae3c3c8cfd3bdf25432
Tansil011019 <13521054@std.stei.itb.ac.id>
2024-03-22 01:37:33 +0700

    feat: add database seeder

--- app/src/main/java/com/example/bondoman/framework/RoomTransactionDataSource.kt
+++ app/src/main/java/com/example/bondoman/framework/RoomTransactionDataSource.kt
@@ -5,9 +5,32 @@ import com.example.bondoman.core.data.Transaction
 import com.example.bondoman.core.repository.TransactionDataSource
 import com.example.bondoman.framework.db.DatabaseService
 import com.example.bondoman.framework.db.TransactionEntity
+import com.example.bondoman.lib.transaction.TRANSACTION_TYPE
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class RoomTransactionDataSource(context: Context) : TransactionDataSource {
     val transactionDao = DatabaseService.getInstance(context).transactionDao()
+    init {
+        CoroutineScope(Dispatchers.IO).launch {
+            seedDatabase(context)
+        }
+    }
+
+    private suspend fun seedDatabase(context: Context) {
+        if (transactionDao.getAllTransactionEntities().isEmpty()) {
+            val transactions = listOf(
+                TransactionEntity("Transaction 1", TRANSACTION_TYPE.PEMASUKAN, 100, System.currentTimeMillis(), "Location 1"),
+                TransactionEntity("Transaction 2", TRANSACTION_TYPE.PEMBELIAN, 200, System.currentTimeMillis(), "Location 2")
+            )
+
+            transactions.forEach { transaction ->
+                transactionDao.addTransactionEntity(transaction)
+            }
+        }
+    }
+
 
     override suspend fun add(transaction: Transaction) = transactionDao.addTransactionEntity(
         TransactionEntity.fromTransaction(transaction)
@@ -17,6 +40,8 @@ class RoomTransactionDataSource(context: Context) : TransactionDataSource {
 
 
     override suspend fun getAll() = transactionDao.getAllTransactionEntities().map{it.toTransaction()}
+
+    override suspend fun getTransactionTypeCount(type: TRANSACTION_TYPE) = transactionDao.getTransactionTypeCount(type)
 
     override suspend fun remove(transaction: Transaction) = transactionDao.deleteTransactionEntity(
         TransactionEntity.fromTransaction(transaction))
