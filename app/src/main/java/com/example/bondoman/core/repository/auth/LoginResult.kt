bc8373dc2634b1b9115e4c0bd4dfdaacdf222ccc
Tansil011019 <13521054@std.stei.itb.ac.id>
2024-03-31 03:36:46 +0700

    feat: add token service

--- app/src/main/java/com/example/bondoman/core/repository/auth/LoginResult.kt
+++
@@ -1,8 +0,0 @@
-package com.example.bondoman.core.repository.auth
-
-import com.example.bondoman.api.auth.login.dto.LoginResponse
-
-sealed class LoginResult {
-    data class Success(val response: LoginResponse) : LoginResult()
-    data class Error(val response: String) : LoginResult()
-}
