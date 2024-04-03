9abd91119f9c99bf6fad4aae5125a7892b641bb8
frankiehuangg <13521092@std.stei.itb.ac.id>
2024-04-04 02:07:07 +0700

    feat: implement save spreadsheet and send email

--- app/src/main/java/com/example/bondoman/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/example/bondoman/ui/settings/SettingsViewModel.kt
@@ -1,7 +1,51 @@
 package com.example.bondoman.ui.settings
 
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.bondoman.core.data.Transaction
+import com.example.bondoman.core.repository.TransactionRepository
+import com.example.bondoman.core.usecase.AddTransaction
+import com.example.bondoman.core.usecase.GetAllTransaction
+import com.example.bondoman.core.usecase.GetTransaction
+import com.example.bondoman.core.usecase.GetTransactionTypeCount
+import com.example.bondoman.core.usecase.RemoveTransaction
+import com.example.bondoman.framework.RoomTransactionDataSource
+import com.example.bondoman.framework.UseCases
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class SettingsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class SettingsViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+    val repository = TransactionRepository(RoomTransactionDataSource(application))
+
+    val useCases = UseCases(
+        AddTransaction(repository),
+        GetTransaction(repository),
+        GetAllTransaction(repository),
+        RemoveTransaction(repository),
+        GetTransactionTypeCount(repository),
+    )
+
+    val transactions = MutableLiveData<List<Transaction>>()
+
+    fun getTransactions() {
+        coroutineScope.launch {
+            val transactionList = useCases.getAllTransaction()
+            Log.d("Settings View Model", transactionList.toString())
+            transactions.postValue(transactionList)
+            Log.d("Settings View Model", transactions.value.toString())
+        }
+    }
+
+    fun setTransactions(transactionList: List<Transaction>) {
+        transactions.value = transactionList
+    }
 }
