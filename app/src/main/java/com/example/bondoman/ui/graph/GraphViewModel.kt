3241ac329ebc9686a8d1bef69d74f6872340d4df
Tansil011019 <13521054@std.stei.itb.ac.id>
2024-04-05 04:48:57 +0700

    fix: graph auto change

--- app/src/main/java/com/example/bondoman/ui/graph/GraphViewModel.kt
+++ app/src/main/java/com/example/bondoman/ui/graph/GraphViewModel.kt
@@ -2,11 +2,6 @@ package com.example.bondoman.ui.graph
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import com.anychart.chart.common.dataentry.DataEntry
-import com.anychart.chart.common.dataentry.ValueDataEntry
 import com.example.bondoman.core.repository.TransactionRepository
 import com.example.bondoman.core.usecase.AddTransaction
 import com.example.bondoman.core.usecase.GetAllTransaction
@@ -16,18 +11,10 @@ import com.example.bondoman.core.usecase.RemoveTransaction
 import com.example.bondoman.framework.RoomTransactionDataSource
 import com.example.bondoman.framework.UseCases
 import com.example.bondoman.lib.transaction.TRANSACTION_TYPE
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class GraphViewModel(application: Application):AndroidViewModel(application) {
-
-    private val _countData = MutableLiveData<List<DataEntry>>()
-
-    val countData: LiveData<List<DataEntry>> = _countData
-
-    private val coroutineScope = CoroutineScope(Dispatchers.IO)
 
     val repository = TransactionRepository(RoomTransactionDataSource(application))
 
@@ -39,18 +26,11 @@ class GraphViewModel(application: Application):AndroidViewModel(application) {
         GetTransactionTypeCount(repository)
     )
 
-    fun getCountTypeData(){
-        coroutineScope.launch {
-            val pemasukanCount:Int = useCases.getTransactionTypeCount(TRANSACTION_TYPE.PEMASUKAN)
-            val pembelianCount:Int = useCases.getTransactionTypeCount(TRANSACTION_TYPE.PEMBELIAN)
-
-            val dataEntries:List<DataEntry> = listOf(
-                    ValueDataEntry("Pemasukan", pemasukanCount),
-                    ValueDataEntry("Pembelian", pembelianCount))
-
-            withContext(Dispatchers.Main) {
-                _countData.value = dataEntries
-            }
+    suspend fun getCountTypeData(): Pair<Int, Int>{
+        return withContext(Dispatchers.Default) {
+            val pemasukanCount = useCases.getTransactionTypeCount(TRANSACTION_TYPE.PEMASUKAN)
+            val pembelianCount = useCases.getTransactionTypeCount(TRANSACTION_TYPE.PEMBELIAN)
+            Pair(pemasukanCount, pembelianCount)
         }
     }
 }
